4c12e519558f5ba1e5a0ab1d85c0c8d1fcc42d65
Jenorg <jenfermz44@gmail.com>
2025-06-06 22:50:30 -0500

    feat: :tada: Hello world!

---
+++ README.md
@@ -0,0 +1,2 @@
+# Portafolio personal
+¡Bienvenido/a a mi portafolio personal! Aquí muestro algunos de mis proyectos, habilidades y experiencia como desarrollador/a frontend.
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-full h-dvh bg-black grid place-items-center">
+      <h1 className='text-white text-3xl'>Hola mundo!</h1>
+    </div>
+  )
+}
+
+export default App
